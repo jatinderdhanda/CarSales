0ca71c92afe6b9e290c7d3738a95aab37a87aa6e
RavneetKaurWex <ravneet.kaur@wexinc.com>
2024-06-19 16:06:18 +1000

    handler to get booking detail

---
+++ CarSales.Application/Queries/Booking/GetBookingDetails/GetBookingDetailQuery.cs
@@ -0,0 +1,5 @@
+﻿using CarSales.Application.Abstractions.Messaging;
+using CarSales.Domain.Models;
+
+namespace CarSales.Application.Queries.Booking.GetBookingDetails;
+public sealed record GetBookingDetailQuery(Guid CarId) : IQuery<IEnumerable<BookingResponse>>;
---
+++ CarSales.Application/Queries/Booking/GetBookingDetails/GetBookingDetailQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using CarSales.Application.Abstractions.Messaging;
+using CarSales.Domain.Abstraction;
+using CarSales.Domain.Interfaces;
+using CarSales.Domain.Models;
+
+namespace CarSales.Application.Queries.Booking.GetBookingDetails;
+internal sealed class GetBookingDetailQueryHandler : IQueryHandler<GetBookingDetailQuery, IEnumerable<BookingResponse>>
+{
+    private readonly IBookingRepository _bookingRepository;
+    public GetBookingDetailQueryHandler(IBookingRepository bookingRepository)
+    {
+        _bookingRepository = bookingRepository;
+    }
+
+    public async Task<Result<IEnumerable<BookingResponse>>> Handle(GetBookingDetailQuery request, CancellationToken cancellationToken)
+    {
+        var bookingDetail = await _bookingRepository.GetAllBookingsForCarIdAsync(request.CarId);
+        return Result.Success(bookingDetail);
+    }
+}
